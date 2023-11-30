e76ac2f8208ce5b2f08a7731d478507951e39c5d
BoitumeloRamasike <ramasikeboitumelo@icloud.com>
2023-12-01 00:09:54 +0200

    updated install munge

--- InstallMunge.sh
+++ InstallMunge.sh
@@ -13,8 +13,9 @@ chmod 600 /etc/munge/munge.key
 
 #copy the MUNGE key to your compute node to allow it to authenticate
 #create this directory in wn's : /etc/munge
-scp /etc/munge/munge.key wn01:/etc/munge/munge.key
-scp /etc/munge/munge.key wn02:/etc/munge/munge.key
+#scp /etc/munge/munge.key wn01:/etc/munge/munge.key
+#scp /etc/munge/munge.key wn02:/etc/munge/munge.key
 
 systemctl start munge
 systemctl enable munge
+systemctl status munge
