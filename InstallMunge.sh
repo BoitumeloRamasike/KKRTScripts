d5a2308a34bc60ea0e58e6110c9671c8b95aa9f8
BoitumeloRamasike <ramasikeboitumelo@icloud.com>
2023-11-30 23:09:14 +0200

    install munge

---
+++ InstallMunge.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+#install epel-release
+dnf install epel-release
+
+#install munge
+dnf -y install munge munge-libs
+
+#Generate a MUNGE key for client authentication
+/usr/bin/create-munge-key
+chown munge:munge /etc/munge/munge.key
+chmod 600 /etc/munge/munge.key
+
+#copy the MUNGE key to your compute node to allow it to authenticate
+#create this directory in wn's : /etc/munge
+scp /etc/munge/munge.key wn01:/etc/munge/munge.key
+scp /etc/munge/munge.key wn02:/etc/munge/munge.key
+
+systemctl start munge
+systemctl enable munge
